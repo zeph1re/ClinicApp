9254f92a9c9a8087f56e6ea08b2e4a1fcd7743f5
Arlinno Ganda Saputra <2242675@diy01.transcosmos-id.local>
2025-10-02 17:01:41 +0700

    fix auth and add doctor detail and approval modal

--- js/auth.js
+++ js/auth.js
@@ -4,15 +4,14 @@ import {supabaseUrl, supabaseAnonKey} from './constant.js';
 const supabase = supabase.createClient(supabaseUrl, supabaseAnonKey);
 
 // Sign Up
-document.getElementById('registerForm').addEventListener('submit', async function (e) {
-  e.preventDefault();
+document.getElementById('registerForm').addEventListener('submit', async (event) => {
+  event.preventDefault();
 
   // === 3. Get form values ===
   const fullName = document.getElementById('fullName').value.trim();
   const email = document.getElementById('email').value.trim();
   const password = document.getElementById('password').value;
   const confirmPassword = document.getElementById('confirmPassword').value;
-  const role = document.getElementById('role').value;
 
   // === 4. Validate ===
   if (!fullName || !email || !password || !confirmPassword) {
@@ -26,49 +25,57 @@ document.getElementById('registerForm').addEventListener('submit', async functio
   }
 
   // === 5. Sign up with Supabase ===
-  const { data, error } = await supabase.auth.signUp({
+  const { user, error } = await supabase.auth.signUp({
     email,
     password,
-    options: {
-      data: {
-        full_name: fullName,
-        role: role
-      }
-    }
   });
 
   if (error) {
     alert("Registration failed: " + error.message);
-    return;
+  } else {
+    await supabase
+    .from('users')
+    .insert([{email: email, fullName: fullName, role: 'patient'}]);
+
+    alert("Registration successful! Please check your email to confirm your account.");
+  
+    // Optionally redirect to login page
+    window.location.href = 'login.html';
   }
 
-  alert("Registration successful! Please check your email to confirm your account.");
   
-  // Optionally redirect to login page
-  window.location.href = 'auth/login.html';
 });
 
 
 // Login 
 
 const loginForm = document.getElementById('loginForm');
-loginForm.addEventListener('submit', async function(){
-    e.preventDefault();
+loginForm.addEventListener('submit', async (event) => {
+    event.preventDefault();
 
     const email = document.getElementById('email')?.value.trim();
     const password = document.getElementById('password');
 
-    const {data, error} = await supabase.auth.signInWithPassword({
+    const {user, error} = await supabase.auth.signInWithPassword({
         email, password
     });
 
     if(error) {
         alert("Login Error = " + error.message);
-        return;
+    } else {
+      document.getElementById('auth-status').innerText = `Welcome, ${user.fullName}`;
+
+      const {data, error: userDataError} = await supabase.from('users').select('*').eq('uid', user.id).single();
+
+      if(userDataError) {
+        alert(userDataError.message);
+      } else {
+        console.log('User Data', data);
+      }
     }
 
     if (role === 'patient') {
-        window.location.href = '/index.html';
+        window.location.href = 'index.html';
     } if (role === "admint") {
         window.location.href = 'admin.html';
     } else {
